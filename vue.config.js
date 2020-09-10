5d9b10643822be65512017003681b8b0466033da
simplephi <cleverdarkness@gmail.com>
2020-09-10 22:44:47 +0800

    tambah dinas a

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+//module.exports = {
+//    baseUrl: '',
+//    outputDir: 'dist',
+//    devServer: {
+//        proxy: 'http://localhost:5000'
+//    }
+//}
+    
+module.exports = {
+	baseUrl: '',
+	outputDir: 'dist',
+}
